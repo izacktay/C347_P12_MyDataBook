158cc78c024fdfd1d5ed2a72d6990e02b8c49fc9
15056215 <15056215@myrp.edu.sg>
2017-08-11 13:28:32 +0800

    Updated

--- app/src/main/java/sg/edu/rp/c347/c347_p12_mydatabook/BioFragment.java
+++ app/src/main/java/sg/edu/rp/c347/c347_p12_mydatabook/BioFragment.java
@@ -1,11 +1,20 @@
 package sg.edu.rp.c347.c347_p12_mydatabook;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -13,17 +22,67 @@ import android.view.ViewGroup;
  */
 public class BioFragment extends Fragment {
 
-
-    public BioFragment() {
-        // Required empty public constructor
-    }
+    Button btnEdit;
+    TextView tv;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bio, container, false);
+        View view = inflater.inflate(R.layout.fragment_bio, container, false);
+
+        tv = (TextView) view.findViewById(R.id.textView);
+        btnEdit = (Button) view.findViewById(R.id.buttonEdit);
+
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                LayoutInflater inflate1 = LayoutInflater.from(getContext());
+
+
+                LinearLayout passPhrase =
+                        (LinearLayout) inflate1.inflate(R.layout.pass_phrase, null);
+                final EditText etPassphrase = (EditText) passPhrase.findViewById(R.id.editTextPassPhrase);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Please Enter")
+                        .setView(passPhrase)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                //This is to get the text from the Passphrase and display a toast message.
+                                Toast.makeText(getContext(), "You had entered " +
+                                        etPassphrase.getText().toString(), Toast.LENGTH_LONG).show();
+
+                                tv.setText(etPassphrase.getText().toString());
+
+                            }
+                        })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(), "You clicked cancle", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+
+        });
+
+        return view;
+
     }
+
+
 
 }
